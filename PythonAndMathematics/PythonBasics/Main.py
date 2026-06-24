675d1edbe1db92056b1f29e801800afa10566936
Umair Ali <umairmamoor@gmail.com>
2026-06-24 19:03:55 +0800

    Learn Python fundamentals: data types, variables, operators, binary conversion, and basic math functions

--- PythonAndMathematics/PythonBasics/Main.py
+++ PythonAndMathematics/PythonBasics/Main.py
@@ -37,3 +37,40 @@ print(round(3.1)) #3
 print(abs(-20)) #20
 
 
+#Complex is for complex math problems
+
+#Binary representation
+print(bin(5))
+#returning binary into int
+print(int('0b101', 2))
+
+#Python variables
+#stores information
+iq = 190
+print(iq)
+user_iq = 190 # snake case
+_user_iq = 190 # a variable can start with lower case or underscore
+#variables are case-sensitive
+#don't overwrite variables - print(print)
+
+#Constants are in capitals
+PI = 3.14 # this value should never change
+a,b,c = 1,2,3
+print(a)
+print(b)
+print(c)
+
+#Expressions vs statement
+iq = 100
+user_age = iq / 5 #iq / 5 is an expression
+user_ages = iq / 5 # this whole line is an expression
+
+# augmented assignment operator
+some_user = 5
+some_user += 2
+some_user -= 2
+some_user *= 2
+print(some_user)
+
+
+
