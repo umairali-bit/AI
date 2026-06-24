e5ec632c313e1264828150a013a0f2b3aa1323bd
Umair Ali <umairmamoor@gmail.com>
2026-06-24 18:41:53 +0800

    Python Basics

---
+++ PythonAndMathematics/PythonBasics/Main.py
@@ -0,0 +1,39 @@
+
+#name = input('What is your name? ')
+#print('Hello ' + name)
+
+#Fundamental Data Types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+
+#Classes -> custom types
+#SuperCar
+
+#Specialized Data Types
+#Modules
+
+None # ->  means nothing like zero
+
+
+int # A number
+print(type(2 + 4)) #type shows the data type
+
+float
+print(type(20+1.0))
+
+print (2 ** 2) # = 4 its 2 to the power of 2
+print (7 / 2) # = 3.5
+print (7 // 2) # = 3 Floor division returns the integer part after rounding down.
+
+#math functions
+print(round(3.1)) #3
+print(abs(-20)) #20
+
+
