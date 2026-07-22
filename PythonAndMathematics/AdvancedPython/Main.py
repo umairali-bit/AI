c5859cdcb2ce3eec176c82d94f4abf7497b20daf
Umair Ali <umairmamoor@gmail.com>
2026-07-22 08:44:39 +0500

    Advanced Python: Method resolution order, multiple inheritance, dunder methods, Super()

--- PythonAndMathematics/AdvancedPython/Main.py
+++ PythonAndMathematics/AdvancedPython/Main.py
@@ -150,6 +150,138 @@ def in_class(user):
 print(in_class(student))
 print(in_class(teacher))
 
+#pet exercise
+
+# Exercise Pets Everywhere
+
+class Pets():
+    animals = []
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+class Cat():
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f'{self.name} is just walking around'
+
+class Simon(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+class Sally(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+class Oliver(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+my_cats =[Simon(Simon, 5), Sally(Sally, 4), Oliver(Oliver, 6)]
+
+my_pets = Pets(my_cats)
+
+my_pets.walk()
+
+#Super()
+class Employee:
+    def __init__(self, name, age, email):
+        self._name = name
+        self._age = age
+        self._email = email
+
+    def signin(self):
+        return 'signed in'
+
+
+class Manager(Employee):
+    def __init__(self, name, age, email):
+        super().__init__( name, age, email)
+
+    def work(self):
+       return f'Manager {self._name} is {self._age} years old, and his email is {self._email}. He is {self.signin()}.'
+
+Manager1 = Manager(name='Gus Fringe', age=57, email = 'gus@unknown.com')
+print(Manager1.work())
+# introspection
+print(dir(Manager1.work()))
+
+
+# dunder methods
+class Money:
+    def __init__(self, amount):
+        self._amount = amount
+
+    def add(self, other):
+       return Money(self._amount + other._amount)
+
+    def __str__(self):
+        return str(self._amount)
+
+a = Money(10)
+b = Money(20)
+
+print(a.add(b))
+
+#exercise
+
+class SuperList(list):
+    def __len__(self):
+        return 1000
+
+superList1 = SuperList()
+print(len(superList1))
+superList1.append(10)
+superList1.append(5)
+print(superList1[0])
+print(superList1[1])
+print(issubclass(SuperList, list))
+
+#Multiple inheritance
+class Person:
+    def introduce(self):
+        return 'Hi I am also a person'
+
+class EmailMixin:
+    def send_email(self):
+        return "Sending email..."
+
+class Engineer(Person, EmailMixin):
+    pass
+
+engineer = Engineer()
+print(engineer.introduce())
+print(engineer.send_email())
+
+#MRO - Method resolution order
+class A:
+    num = 10
+
+class B(A):
+    pass
+
+class C(A):
+    num = 1
+
+class D(B, C):
+    pass
+
+print(D.num)
+print(D.__mro__)
+
+
+
+
+
+
 
 
 
